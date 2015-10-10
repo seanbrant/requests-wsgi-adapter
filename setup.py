3195d36a882d6fe047c0b5be5ec8da394bb4c111
Sean Brant <brant.sean@gmail.com>
2015-10-10 17:05:59 -0500

    Use correct attribute for single module package

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
@@ -11,9 +11,7 @@ setup(
     author_email='brant.sean@gmail.com',
     url='https://github.com/seanbrant/requests-wsgi-adapter',
     license='BSD',
-    packages=find_packages(exclude=['tests.py']),
-    include_package_data=True,
-    zip_safe=False,
+    py_modules=['wsgiadapter'],
     test_suite='runtests.runtests',
     install_requires=[
         'requests>=1.0',
