6147f3e52d1f467b7d0c0d2dd9f5bef5bccf2f3e
Sean Brant <brant.sean@gmail.com>
2016-06-20 23:42:27 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='requests-wsgi-adapter',
-    version='0.1.4',
+    version='0.1.5',
     description='WSGI Transport Adapter for Requests',
     long_description=open('README.rst').read(),
     author='Sean Brant',
