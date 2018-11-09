cdab8f2d3ceb0cbc6aee60fd8194af5933e677d2
Андрей Парамонов <paramon@lims.acdlabs.ru>
2018-11-09 12:52:39 +0300

    Make environ['SERVER_PORT'] str consistently

--- wsgiadapter.py
+++ wsgiadapter.py
@@ -108,7 +108,7 @@ class WSGIAdapter(BaseAdapter):
             'REQUEST_METHOD': request.method,
             'SERVER_NAME': urlinfo.hostname,
             'QUERY_STRING': urlinfo.query,
-            'SERVER_PORT': urlinfo.port or ('443' if urlinfo.scheme == 'https' else '80'),
+            'SERVER_PORT': str(urlinfo.port or (443 if urlinfo.scheme == 'https' else 80)),
             'SERVER_PROTOCOL': self.server_protocol,
             'wsgi.version': self.wsgi_version,
             'wsgi.input': Content(data),
